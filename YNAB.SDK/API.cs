f631b67716eaedc1c510670de6c3bf381537c03a
Jake Moening <jmoening@imagetrend.com>
2019-08-23 09:23:16 -0500

    Add API top level element to mirror Ruby/JS SDKs

---
+++ YNAB.SDK/API.cs
@@ -0,0 +1,28 @@
+using YNAB.SDK.Client;
+using YNAB.SDK.Api;
+
+namespace YNAB.SDK
+{
+  public class API {
+    private Configuration _config = new Configuration();
+    private string _host;
+    private bool _useHttps;
+    public API(string accessToken, string host = "api.youneedabudget.com", bool useHttps = true)
+    {
+      _host = host;
+      _useHttps = useHttps;
+      _config.AddApiKey("Authorization", accessToken);
+      _config.AddApiKeyPrefix("Authorization", "Bearer");
+    }
+
+    public UserApi User { get { return new UserApi(_config); } }
+    public BudgetsApi Budgets { get { return new BudgetsApi(_config); } }
+    public AccountsApi Accounts { get { return new AccountsApi(_config); } }
+    public CategoriesApi Categories { get { return new CategoriesApi(_config); } }
+    public MonthsApi Months { get { return new MonthsApi(_config); } }
+    public PayeesApi Payees { get { return new PayeesApi(_config); } }
+    public PayeeLocationsApi PayeesLocations { get { return new PayeeLocationsApi(_config); } }
+    public TransactionsApi Transactions { get { return new TransactionsApi(_config); } }
+    public ScheduledTransactionsApi ScheduledTransactions { get { return new ScheduledTransactionsApi(_config); } }
+  }
+}
